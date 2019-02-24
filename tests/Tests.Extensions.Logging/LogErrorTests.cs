c5078eff97c41ea53fad4ed2c57eae41f89529ea
Renato Golia <golia.renato@gmail.com>
2019-02-24 14:26:43 +0100

    Updated for v2 (#1)
    - Now targets netstandard2.0 and net461
    - Added test project
    - CI/CD
      - Uses Cake to build
      - Uses GitVersion to calculate current version
      - Added appveyor.yml file

---
+++ tests/Tests.Extensions.Logging/LogErrorTests.cs
@@ -0,0 +1,94 @@
+using NUnit.Framework;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+
+// ReSharper disable InvokeAsExtensionMethod
+
+namespace Tests
+{
+    [TestFixture]
+    public class LogErrorTests
+    {
+        private static Func<TState, Exception, string> AnyFormatter<TState>() => It.IsAny<Func<TState, Exception, string>>();
+
+        private static void SetUpLogger<TState>(ILogger logger)
+        {
+            Mock.Get(logger)
+                .Setup(p => p.Log(It.IsAny<LogLevel>(), It.IsAny<EventId>(), It.IsAny<TState>(), It.IsAny<Exception>(), It.IsAny<Func<TState, Exception, string>>()))
+                .Callback<LogLevel, EventId, TState, Exception, Func<TState, Exception, string>>((l, ev, s, e, f) => f(s, e));
+        }
+
+        [Test, AutoMoqData]
+        public void LogError_logs_message(ILogger logger, string message)
+        {
+            LoggerExtraExtensions.LogError(logger, message);
+            
+            Mock.Get(logger).Verify(p => p.Log(LogLevel.Error, 0, message, null, AnyFormatter<string>()));
+        }
+
+        [Test, AutoMoqData]
+        public void LogError_logs_exception(ILogger logger, Exception error)
+        {
+            LoggerExtraExtensions.LogError(logger, error);
+
+            Mock.Get(logger).Verify(p => p.Log(LogLevel.Error, 0, It.IsAny<object>(), error, AnyFormatter<object>()));
+        }
+
+        [Test, AutoMoqData]
+        public void LogError_logs_state(ILogger logger, TestState state, Func<TestState, string> formatter)
+        {
+            SetUpLogger<TestState>(logger);
+
+            LoggerExtraExtensions.LogError(logger, state, formatter);
+
+            Mock.Get(logger).Verify(p => p.Log(LogLevel.Error, 0, state, null, AnyFormatter<TestState>()));
+
+            Mock.Get(formatter).Verify(p => p(state));
+        }
+
+        [Test, AutoMoqData]
+        public void LogError_logs_state_with_event(ILogger logger, TestState state, EventId eventId, Func<TestState, string> formatter)
+        {
+            SetUpLogger<TestState>(logger);
+
+            LoggerExtraExtensions.LogError(logger, eventId, state, formatter);
+
+            Mock.Get(logger).Verify(p => p.Log(LogLevel.Error, eventId, state, null, AnyFormatter<TestState>()));
+
+            Mock.Get(formatter).Verify(p => p(state));
+        }
+
+        [Test, AutoMoqData]
+        public void LogError_logs_state_with_error(ILogger logger, TestState state, Exception error, Func<TestState, Exception, string> formatter)
+        {
+            SetUpLogger<TestState>(logger);
+
+            LoggerExtraExtensions.LogError(logger, state, error, formatter);
+
+            Mock.Get(logger).Verify(p => p.Log(LogLevel.Error, 0, state, error, AnyFormatter<TestState>()));
+
+            Mock.Get(formatter).Verify(p => p(state, error));
+        }
+
+        [Test, AutoMoqData]
+        public void LogError_logs_state_with_error_and_event(ILogger logger, TestState state, EventId eventId, Exception error, Func<TestState, Exception, string> formatter)
+        {
+            SetUpLogger<TestState>(logger);
+
+            LoggerExtraExtensions.LogError(logger, eventId, state, error, formatter);
+
+            Mock.Get(logger).Verify(p => p.Log(LogLevel.Error, eventId, state, error, AnyFormatter<TestState>()));
+
+            Mock.Get(formatter).Verify(p => p(state, error));
+        }
+
+        [Test, AutoMoqData]
+        public void LogError_logs_state_with_error_and_event(ILogger logger, EventId eventId, Exception error)
+        {
+            LoggerExtraExtensions.LogError(logger, eventId, error);
+
+            Mock.Get(logger).Verify(p => p.Log(LogLevel.Error, eventId, It.IsAny<object>(), error, AnyFormatter<object>()));
+        }
+    }
+}
